dece2d9e989d402e4bc8688e273c034fe1a6e538
Palak <98663148+palak-7@users.noreply.github.com>
2024-11-29 16:32:40 +0530

    add new database and blog

--- src/app/blogs/Blog.jsx
+++ src/app/blogs/Blog.jsx
@@ -62,11 +62,13 @@ const Blog = () => {
                 <h2 className="text-xl font-semibold mb-2">{n.name}</h2>
                 <div className="flex gap-3">
                   <SlCalender />
-                  <p className="text-gray-800 text-sm mb-4"><Moment format="MMMM DD, YYYY">{n.date}</Moment></p>
+                  <p className="text-gray-800 text-sm mb-4">
+                    <Moment format="MMMM DD, YYYY">{n.date}</Moment>
+                  </p>
                 </div>
                 <p
                   className="text-gray-700 line-clamp-3"
-                  dangerouslySetInnerHTML={{ __html: n.description}}
+                  dangerouslySetInnerHTML={{ __html: n.description }}
                 ></p>
                 <Link href={`/blogs/${n.url}`} className="flex mb-3 py-4">
                   <div className="bg-[#07a496] p-3 text-white rounded-xl">
--- src/app/blogs/Sidebar.jsx
+++ src/app/blogs/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
       <div className="sm:px-6 lg:px-8 border-2 rounded-2xl h-screen">
         <ul className="h-screen">
           <div className="">
-            <p className="text-[#07a496] font-semibold text-2xl py-2 ">
+            <p className="text-[#07a496] font-semibold text-xl py-2 ">
               More Blogs
             </p>
           </div>
@@ -40,10 +40,10 @@ const Sidebar = () => {
                   className="flex hover:translate-x-2 transition duration-300 ease-in-out"
                 >
                   <div className="flex gap-1 hover:text-[#07a496]">
-                    <div className="text-2xl">
+                    <div className="text-xl">
                       <AiOutlineDoubleRight className="mt-1" />
                     </div>
-                    <h2 className="text-xl font-medium">{n.short_name}</h2>
+                    <h2 className="text-lg font-medium">{n.short_name}</h2>
                   </div>
                 </Link>
               </div>
