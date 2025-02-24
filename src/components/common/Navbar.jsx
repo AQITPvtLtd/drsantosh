ab8635d1238ba8d8d2e77bcd981ed37eddce6fc1
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-02-24 18:08:00 +0530

    add shadow

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -17,8 +17,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="fixed lg:top-[68px] xl:top-10 top-0 left-0 w-full bg-white dark:bg-white z-40">
-        {/* Adjusted the top property to 12 to sit below the Top component */}
+      <nav className="fixed lg:top-[68px] xl:top-10 top-0 left-0 w-full bg-white dark:bg-white z-40" style={{ boxShadow: '0px 25px 20px -20px rgba(0,0,0,0.45)' }}>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2 dark:text-white">
           <Link
             href="/"
@@ -60,9 +59,8 @@ const Navbar = () => {
             </button>
           </div>
           <div
-            className={`items-center justify-between ${
-              isMenuOpen ? "block" : "hidden"
-            } w-full md:flex md:w-auto md:order-1`}
+            className={`items-center justify-between ${isMenuOpen ? "block" : "hidden"
+              } w-full md:flex md:w-auto md:order-1`}
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-white dark:text-black">
