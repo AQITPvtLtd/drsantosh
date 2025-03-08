da6377265dd0c865795a67ea247858781918fafe
AQITPvtLtd <tekbooster25@gmail.com>
2025-03-08 16:20:40 +0530

    new correction

--- src/app/services/[id]/ServiceData.jsx
+++ src/app/services/[id]/ServiceData.jsx
@@ -45,7 +45,7 @@ const ServiceData = ({ id }) => {
                             <div>
                                 {/* Causes List */}
                                 <h2 className="text-2xl font-semibold text-gray-900 mb-2">Causes of Diabetes</h2>
-                                <ul className="list-disc list-inside text-gray-700">
+                                <ul className="list-disc list-outside text-gray-700">
                                     {obj.causes.map((cause, index) => (
                                         <li key={index} className="mb-2">{cause}</li>
                                     ))}
@@ -55,7 +55,7 @@ const ServiceData = ({ id }) => {
                             <div>
                                 {/* Prevention List */}
                                 <h2 className="text-2xl font-semibold text-gray-900 mb-2">Prevention of Diabetes</h2>
-                                <ul className="list-disc list-inside text-gray-700">
+                                <ul className="list-disc list-outside text-gray-700">
                                     {obj.prevention.map((step, index) => (
                                         <li key={index} className="mb-2">{step}</li>
                                     ))}
